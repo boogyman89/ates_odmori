d77165e50a555992f18f51d6b06929ce832cc7bb
aleksandar89 <accca10@gmail.com>
2013-07-31 09:11:58 +0200

    - Nova migracija (ukinuta veza user-holiday, preimenovana tabela ates_user i drop table slava. - Ubaceno logovanje po email-u umesto username-a.

--- src/Ates/UserBundle/Controller/DefaultController.php
+++ src/Ates/UserBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('AtesUserBundle:Default:index.html.twig', array('name' => 'Nenad'));
+        return $this->render('AtesUserBundle:Default:index.html.twig', array('name' => 'guys! It\'s working!'));
     }
 }
